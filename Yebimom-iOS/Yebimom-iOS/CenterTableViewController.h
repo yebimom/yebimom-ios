8f25be07450f896114b845d912f37e4f99a8ca92
wikibootup <wikibootup@gmail.com>
2015-04-16 17:19:22 +0900

    add controller : CenterTableViewController

---
+++ Yebimom-iOS/Yebimom-iOS/CenterTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CenterTableViewController.h
+//  Yebimom-iOS
+//
+//  Created by wikibootup on 4/16/15.
+//  Copyright (c) 2015 wikibootup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CenterTableViewController : UITableViewController
+
+@end
