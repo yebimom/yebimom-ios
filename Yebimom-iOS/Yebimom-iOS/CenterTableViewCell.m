353db8d10a8825af54b2784347b5bac18cdf7ebb
wikibootup <wikibootup@gmail.com>
2015-04-16 17:25:53 +0900

    add ViewCell : CenterTableViewCell

---
+++ Yebimom-iOS/Yebimom-iOS/CenterTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CenterTableViewCell.m
+//  Yebimom-iOS
+//
+//  Created by wikibootup on 4/16/15.
+//  Copyright (c) 2015 wikibootup. All rights reserved.
+//
+
+#import "CenterTableViewCell.h"
+
+@implementation CenterTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
