6170881710f291320f6e66e8778a52d31b10d10d
wikibootup <wikibootup@gmail.com>
2015-04-18 20:10:04 +0900

    add controller : CenterDetailViewController

---
+++ Yebimom-iOS/Yebimom-iOS/CenterDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  CenterDetailViewController.h
+//  Yebimom-iOS
+//
+//  Created by wikibootup on 4/18/15.
+//  Copyright (c) 2015 wikibootup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CenterDetailViewController : UIViewController
+
+@end
