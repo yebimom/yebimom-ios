7ab402ebafe1e5af20762a78e68cbea1b5325b45
wikibootup <wikibootup@gmail.com>
2015-06-04 00:00:44 +0900

    add category table cell

---
+++ Yebimom-iOS/Yebimom-iOS/CategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTableViewCell.swift
+//  Yebimom-iOS
+//
+//  Created by wikibootup on 6/3/15.
+//  Copyright (c) 2015 wikibootup. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
