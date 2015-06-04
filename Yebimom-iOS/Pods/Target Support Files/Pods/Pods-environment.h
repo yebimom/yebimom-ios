dffb13342ae25ce1deac73a9d5661cc91195cf63
wikibootup <wikibootup@gmail.com>
2015-06-05 02:17:52 +0900

    add alamofire library : http request library

--- Yebimom-iOS/Pods/Target Support Files/Pods/Pods-environment.h
+++ Yebimom-iOS/Pods/Target Support Files/Pods/Pods-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 2
+#define COCOAPODS_VERSION_PATCH_Alamofire 2
+
 // SwiftyJSON
 #define COCOAPODS_POD_AVAILABLE_SwiftyJSON
 #define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
--- Yebimom-iOS/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Yebimom-iOS/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -48,8 +48,10 @@ code_sign() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework 'Alamofire.framework'
   install_framework 'SwiftyJSON.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework 'Alamofire.framework'
   install_framework 'SwiftyJSON.framework'
 fi
