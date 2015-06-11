1f75fa3eb30a9681841a35d64bbee1fb04bfd8a5
wikibootup <wikibootup@gmail.com>
2015-06-11 20:53:05 +0900

    add facility table view in center detail

---
+++ Yebimom-iOS/Yebimom-iOS/FacilityTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FacilityTableViewCell.swift
+//  Yebimom-iOS
+//
+//  Created by wikibootup on 6/11/15.
+//  Copyright (c) 2015 wikibootup. All rights reserved.
+//
+
+import UIKit
+
+class FacilityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
