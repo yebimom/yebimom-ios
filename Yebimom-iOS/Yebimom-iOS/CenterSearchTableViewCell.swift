3af570bcc76ea7e64a715cd25a84b4c7d0d7f8fb
wikibootup <wikibootup@gmail.com>
2015-06-15 18:53:14 +0900

    connect cell -> storyboard

---
+++ Yebimom-iOS/Yebimom-iOS/CenterSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CenterSearchTableViewCell.swift
+//  Yebimom-iOS
+//
+//  Created by wikibootup on 6/15/15.
+//  Copyright (c) 2015 wikibootup. All rights reserved.
+//
+
+import UIKit
+
+class CenterSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Yebimom-iOS/Yebimom-iOS/CenterSearchTableViewController.swift
+++ Yebimom-iOS/Yebimom-iOS/CenterSearchTableViewController.swift
@@ -26,24 +26,24 @@ class CenterSearchTableViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return 1
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("CenterSearchTableCell", forIndexPath: indexPath) as! CenterSearchTableViewCell
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
